5e715a19abffb44ce2508ea9d828e46ab24fd334
townson <sheryl.townson@gmail.com>
2021-11-30 13:09:55 +1000

    Testing Branch Sheryl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic_automation_duck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic_automation_duck.java
@@ -92,18 +92,24 @@ public class Basic_automation_duck extends LinearOpMode {
 
             leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            spiner.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
             leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            spiner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-            encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-            encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+            //encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+            //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+            //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
+            int move_To_Duck = 20;
+            int move_From_Edge = 10;
+            encoderDrive(DRIVE_SPEED, move_From_Edge,move_From_Edge,3.0 ); //forwards small
+            encoderDrive(TURN_SPEED, -12, 12,4.0);//turn 90 degress left.
+            encoderDrive(DRIVE_SPEED, move_To_Duck, move_From_Edge, 3.0 );//reverse between two distances
 
-
-            //spiner.setPower(spinPower);
-
+            //activate spin device.
+            encoderSpin(DRIVE_SPEED, 30,4.0);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -115,7 +121,8 @@ public class Basic_automation_duck extends LinearOpMode {
 
     public void encoderDrive(double speed,
                              double leftInches, double rightInches,
-                             double timeoutS) {
+                             double timeoutS)
+    {
         int newLeftTarget;
         int newRightTarget;
 
@@ -152,6 +159,58 @@ public class Basic_automation_duck extends LinearOpMode {
                 telemetry.addData("Path2", "Running at %7d :%7d",
                         leftDrive.getCurrentPosition(),
                         rightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+
+    public void encoderSpin(double speed ,double spinamount, double timeoutS)
+    {
+        int newSpinTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newSpinTarget = spiner.getCurrentPosition() + (int) (spinamount * COUNTS_PER_INCH);
+            //newRightTarget = rightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            spiner.setTargetPosition(newSpinTarget);
+            //rightDrive.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            spiner.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            spiner.setPower(Math.abs(speed));
+            //rightDrive.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (spiner.isBusy() /*&& rightDrive.isBusy() */)) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newSpinTarget/*, newRightTarget*/);
+                // telemetry.addData("Path2", "Running at %7d :%7d",
+                //       spiner.getCurrentPosition();
+                //rightDrive.getCurrentPosition());
                 telemetry.update();
             }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Remote_Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Remote_Control.java
@@ -50,8 +50,83 @@ public class Remote_Control extends LinearOpMode {
 
             //determining the power based on degree on angle on joystick
             if (medSpeed <= angle & angle <= -medSpeed){
+                maxSpeed = 0.6;
+            }
+            else if (lowSpeed <= angle & angle <= - lowSpeed){
+                maxSpeed = 0.4;
+            }
+            else{
+                maxSpeed = 0.2;
+            }
 
+            leftPower   = Range.clip(yAxis + xAxis, -maxSpeed, maxSpeed);
+            rightPower  = Range.clip(yAxis - xAxis, -maxSpeed, maxSpeed);
+
+            //makeing the robot face the cardinal directions of the board when buttons pressed
+            if (upArrow == true){
+                gyroTurn(0.6, 0);
+            }
+            if (downArrow == true){
+                gyroTurn(0.6, 90);
+            }
+            if (leftArrow == true){
+                gyroTurn(0.6, 270);
+            }
+            if (rightArrow == true){
+                gyroTurn(0.6, 360);
             }
         }
+    }
+
+    //as given
+    public void gyroTurn(double speed, double angle){
+        while (opModeIsActive() && !onHeading(speed, angle, 0.1)){
+            telemetry.update();
+        }
+    }
+    //as given
+    boolean onHeading(double speed, double angle, double PCoeff){
+        double error;
+        double steer;
+        boolean onTarget = false;
+        double leftSpeed;
+        double rightSpeed;
+
+        error = getError(angle);
+
+        if (Math.abs(error) <= 1){
+            steer = 0;
+            leftSpeed = 0;
+            rightSpeed =0;
+            onTarget = true;
+        }
+        else{
+            steer       = getSteer(error, PCoeff);
+            rightSpeed  = speed * steer;
+            leftSpeed   = -rightSpeed;
+        }
+
+        leftDrive.setPower(leftSpeed);
+        rightDrive.setPower(rightSpeed);
+
+        //add info to display on phone
+        telemetry.addData("Target", "%5.2f", angle);
+        telemetry.addData("Err/St", "%5.2f/%5.2f", error, steer);
+        telemetry.addData("Speed", "%5.2f:%5.2f", leftSpeed, rightSpeed);
+
+        return onTarget;
+    }
+
+    public double getError(double targetAngle){
+        double robotError;
+
+      robotError = 1 ; // targetAngle - gyro.getIntergratedZValue();
+        while(robotError > 180)         robotError -= 360;
+        while(robotError <= 180)        robotError += 360;
+        return robotError;
+    }
+
+    public double getSteer(double error, double PCoeff){
+        return Range.clip(error * PCoeff, -1, 1);
     }
 }
