a3286bef25efbe031a4c8fc6f1dceafbc2a7978d
Natasha Anne Townson <townson.natasha@gmail.com>
2022-02-12 09:29:06 +1000

    Nat 19th cut

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automated_duck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automated_duck.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
-public class DFW extends LinearOpMode{
+public class automated_duck extends LinearOpMode{
     Ninjabot robot;
 
     @Override
@@ -16,25 +16,29 @@ public class DFW extends LinearOpMode{
         int BACKWARD = 3;
         int ROTATE_LEFT = 5;
         int ROTATE_RIGHT = 6;
-
+        //int SPIN = 4;
 
         Ninjabot robot;
         robot = new Ninjabot(hardwareMap, this);
         robot.liftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.spinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
         waitForStart();
         telemetry.addData("Status", "ClawStatus");
         telemetry.update();
-        robot.claw.setPosition(0);
+        robot.claw.setPosition(1);
 
         robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.liftArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.spinner.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 // stall motors
         robot.leftDrive.setTargetPosition(0);
         robot.rightDrive.setTargetPosition(0);
+        robot.spinner.setTargetPosition(0);
 // zero out the motors counters
-
+        robot.spinner.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
@@ -42,45 +46,28 @@ public class DFW extends LinearOpMode{
 
         robot.leftDrive.setPower(0.3);
         robot.rightDrive.setPower(0.3);
+        robot.spinner.setPower(0.5);
 
-//set power for all wheels indefinitely
         //Put moves here
-
-
-            //robot.gyroDrive ( 0.2, 62.0, 0 ) ;
-
-            // drop wooble
-
 
         //robot.driveTo(robot.convert(10), FORWARD);
         //while (!robot.targetReached() && opModeIsActive()) robot.updateWheelTelemetry();
 
-        //robot.claw.setPosition(0.8); //claw open, position 0 is close
-        //sleep(500);
-        //telemetry.addData("Status", "Open");
-        //telemetry.update();
-
-        //robot.driveTo(950, ROTATE_LEFT); //180 degree turn
+            //robot.driveTo(950, ROTATE_LEFT); //180 degree turn
         //while (!robot.targetReached() && opModeIsActive()) robot.updateWheelTelemetry();
 
-        //robot.claw.setPosition(0); //position 0 is close
-        //sleep(500);
-        //telemetry.addData("Status", "close");
-        //telemetry.update();
+        //activate spinner
 
-        robot.liftArm.setTargetPosition(80);
-        robot.liftArm.setPower(1);
-        robot.liftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      //  sleep(2000);
 
-        sleep(2000);
-
-        robot.liftArm.setTargetPosition(20);
-        robot.liftArm.setPower(0.5);
-        robot.liftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //robot.liftArm.setTargetPosition(20);
+        //robot.liftArm.setPower(0.5);
+        //robot.liftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         sleep(2000);
 
-        //robot.driveTo(3000, BACKWARD);
+        robot.driveTo(robot.convert(-15), FORWARD);
+        while (!robot.targetReached() && opModeIsActive()) robot.updateWheelTelemetry();
         //while (!robot.targetReached() && opModeIsActive()) robot.updateWheelTelemetry();
 
         //robot.driveTo(robot.convert(2), ROTATE_RIGHT);
